8396b97da5b42eeeeb3e802dcf0981b7c3615843
marine <63032985+adminpmh@users.noreply.github.com>
2020-05-18 21:56:40 +0900

    FlowControl

---
+++ Java/Cp4-FlowControl/src/ex/DoWhileBasic.java
@@ -0,0 +1,16 @@
+package ex;
+
+public class DoWhileBasic {
+
+	public static void main(String[] args) {
+		
+		int num = 1;	//반복의 조건을 체크하는 변수
+		
+		do {
+			System.out.println("I Like JAVA "+num);
+			num++;
+		}while(num<6);
+
+	}
+
+}
---
+++ Java/Cp4-FlowControl/src/ex/IEBasic.java
@@ -0,0 +1,30 @@
+package ex;
+
+public class IEBasic {
+
+	public static void main(String[] args) {
+		
+		if(true) {
+			System.out.println("true 일때 출력");
+		}
+		if(false) {
+			System.out.println("false 일때 출력");
+		}
+		if(false) {
+			System.out.println("true 일때 출력");
+		}else {
+			System.out.println("false 일때 출력");
+		}
+		if(10>5) {
+			System.out.println("10은 5보다 큼니다");
+		}
+		
+		int num = 10;
+		
+		if(num == 5) {
+			System.out.println("10은 5와 같습니다.");
+		}
+
+	}
+
+}
