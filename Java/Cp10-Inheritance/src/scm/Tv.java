35e3d7b98fcb448acffc406d93e7ee187e96da79
marine <63032985+adminpmh@users.noreply.github.com>
2020-05-25 20:10:36 +0900

    Cp10-Inheritance

---
+++ Java/Cp10-Inheritance/src/scm/Tv.java
@@ -0,0 +1,22 @@
+package scm;
+
+public class Tv extends Product {
+	
+	Tv(int price){
+		super(price);
+	}
+	
+	public String toString() {
+		return "Tv";
+	}
+	
+	void display() {
+		System.out.println("Tv를 시청합니다.");
+	}
+	
+
+}
+
+
+
+
