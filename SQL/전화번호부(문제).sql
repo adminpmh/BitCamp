bf3b0a4c2973b0905a33cb98fa20b66d201621be
marine <hollyzero@naver.com>
2020-05-27 17:54:43 +0900

    Update 전화번호부(문제).sql

--- SQL/전화번호부(문제).sql
+++ SQL/전화번호부(문제).sql
@@ -34,7 +34,7 @@ create table phoneInfo_basic(
     fr_address VARCHAR2(20),
     fr_regdate date DEFAULT sysdate,
     
-    CONSTRAINT phoneInfo_basic_idx_pk primary key(idx)
+    CONSTRAINT phoneInfo_basic_idx_pk primary key(idx) 
 );
 
 create table phoneInfo_univ(
@@ -44,7 +44,7 @@ create table phoneInfo_univ(
     fr_ref number(7)not null,
     
     CONSTRAINT phoneInfo_univ_idx_pk primary key(idx),
-    CONSTRAINT phoneInfo_univ_fr_ref_fk foreign key(fr_ref) references phoneInfo_basic(idx),
+    CONSTRAINT phoneInfo_univ_fr_ref_fk foreign key(fr_ref) references phoneInfo_basic(idx) on delete cascade,
     CONSTRAINT phoneInfo_univ_fr_u_year_ck check(fr_u_year in('1','2','3','4'))
     --CONSTRAINT phoneInfo_univ_fr_u_year_ck check(fr_u_year BETWEEN 1 and 4)
 );
@@ -55,7 +55,7 @@ create table phoneInfo_com(
     fr_ref number(6)not null,
     
     CONSTRAINT phoneInfo_com_idx_pk primary key(idx),
-    CONSTRAINT phoneInfo_com_fr_ref_fk FOREIGN key(fr_ref) references phoneInfo_basic(idx)
+    CONSTRAINT phoneInfo_com_fr_ref_fk FOREIGN key(fr_ref) references phoneInfo_basic(idx) on delete cascade
 );
 
 -----------------------------------------------------
@@ -107,17 +107,101 @@ insert into phoneinfo_basic(idx,FR_NAME,FR_PHONENUMBER,FR_EMAIL,FR_ADDRESS)value
 insert into phoneInfo_univ(idx,FR_U_MAJOR,FR_U_YEAR,FR_REF)values (3,'통일',1,4);
 insert into PHONEINFO_COM(idx,FR_C_COMPANY,FR_REF)values (2,'대한민국',4);
 
+---------------------------------------------------------------
+--수정을 위한 SQL
+---------------------------------------------------------------
+ROLLBACK;
+--1.회사친구의 정보 변경
+update phoneinfo_basic set idx=1,FR_NAME='김택진',FR_PHONENUMBER='010-7777-7777',FR_EMAIL='kimzzang@naver.com',FR_ADDRESS='리니지',FR_REGDATE='' where idx=1;
+update phoneInfo_univ set idx=1,FR_U_MAJOR='게임캐쉬',FR_U_YEAR=1,FR_REF=1 where idx = 1;
+--2.학교친구의 정보 변경
+update phoneinfo_basic set idx=2,FR_NAME='송재경',FR_PHONENUMBER='010-8282-9999',FR_EMAIL='songzzang@gmail.com',FR_ADDRESS='카트라이더',FR_REGDATE='' where idx=2;
+update PHONEINFO_COM set idx=2,FR_C_COMPANY='너돈은내돈',FR_REF=2 where idx=2;
 
 
+---------------------------------------------------------------
+--삭제를 위한 SQL
+---------------------------------------------------------------
+--1.회사친구의 정보 삭제
+delete from phoneInfo_univ where FR_U_MAJOR='게임캐쉬';
+delete from phoneinfo_basic where FR_NAME='김택진';
+
+--2.학교친구의 정보 삭제
+delete from PHONEINFO_COM where FR_U_MAJOR='너돈은내돈';
+delete from phoneinfo_basic where FR_NAME='송재경';
+
+--외래키 설정시 부모의 행이 삭제 될 때 설정 
+--references phoneInfo_basic(idx) on delete 설정 옵션
+--no action : 모두 삭제 불가
+--cascade : 참조를 하고 있는 자식 테이블의 모든 행도 삭제
+--set null : 참조를 하고 있는 자식 테이블의 모든 행의 외래키 칼럼의 값을 null 로 변경 
+--set default :  참조를 하고 있는 자식 테이블의 모든 행의 외래키 칼럼의 값을 기본값으로 변경
+
+---------------------------------------------------------
+--view 생성
+---------------------------------------------------------
 
 
+--1. 전체 친구 목록 출력 : 테이블 3개 JOIN
+select * from PHONEINFO_BASIC pb,PHONEINFO_UNIV pu, PHONEINFO_COM pc
+where pb.idx = pu.fr_ref(+) and pb.idx = pc.fr_ref(+)
+;
+
+--view : pb_all_view
+create or REPLACE view pb_all_view
+as
+select  pb.fr_name,
+        pb.fr_phonenumber,
+        pb.fr_address,
+        pb.fr_email,
+        pu.fr_u_major,
+        pu.fr_u_year,
+        pc.fr_c_company,
+        pb.fr_regdate
+from PHONEINFO_BASIC pb,PHONEINFO_UNIV pu, PHONEINFO_COM pc
+where pb.idx = pu.fr_ref(+) and pb.idx = pc.fr_ref(+)
+;
+select * from pb_all_view;
+
+--2. 학교 친구 목록 출력 
+select * from PHONEINFO_BASIC pb,PHONEINFO_UNIV pu
+where pb.idx = pu.fr_ref
+;
+--view : pb_univ_view
+create or REPLACE view pb_univ_view
+as
+select  pb.fr_name,
+        pb.fr_phonenumber,
+        pb.fr_address,
+        pb.fr_email,
+        pu.fr_u_major,
+        pu.fr_u_year,
+        pb.fr_regdate
+from PHONEINFO_BASIC pb,PHONEINFO_UNIV pu
+where pb.idx = pu.fr_ref
+;
+select * from pb_univ_view;
 
 
+--3. 대학 친구 목록 출력
+select * from PHONEINFO_BASIC pb, PHONEINFO_COM pc
+where pb.idx = pc.fr_ref
+;
 
+--view : pb_com_view
+create or REPLACE view pb_com_view
+as
+select  pb.fr_name,
+        pb.fr_phonenumber,
+        pb.fr_address,
+        pb.fr_email,
+        pc.fr_c_company,
+        pb.fr_regdate
+from PHONEINFO_BASIC pb, PHONEINFO_COM pc
+where pb.idx = pc.fr_ref
+;
 
-
-
-
+select * from pb_com_view;
 
 
 
